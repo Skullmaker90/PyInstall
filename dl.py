cdf080b9acc839e3dd71bef3dc63da735fb3df15
Anthony Smith <Battlefield2guy@cox.net>
2016-04-28 07:31:09 -0700

    Stable 1.1 Release

--- dl.py
+++ dl.py
@@ -2,4 +2,3 @@ import os
 
 os.system('cd /opt && curl -O https://raw.githubusercontent.com/Skullmaker90/PyInstall/master/panelInstall.py')
 os.system('cd /opt && curl -O https://raw.githubusercontent.com/Skullmaker90/PyInstall/master/default.cfg')
-
