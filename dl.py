34f8b9b65004bc6d5e543cad7a35e4ff2db34149
Anthony Smith <asmith@cari.net>
2016-04-28 07:10:29 -0700

    Added dev.

--- dl.py
+++ dl.py
@@ -2,3 +2,4 @@ import os
 
 os.system('cd /opt && curl -O https://raw.githubusercontent.com/Skullmaker90/PyInstall/master/panelInstall.py')
 os.system('cd /opt && curl -O https://raw.githubusercontent.com/Skullmaker90/PyInstall/master/default.cfg')
+
