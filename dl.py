cb25bf7f95ac55dcaf007a90bc46f1c175fecb19
Anthony Smith <asmith@cari.net>
2016-04-27 21:08:03 -0700

    Changed Dl.

--- dl.py
+++ dl.py
@@ -1,3 +1,4 @@
 import os
 
-os.system('cd /home && curl -O https://code.ibizservices.com/asmith/PyInstall/raw/master/panelInstall.py')
+os.system('cd /opt && curl -O https://raw.githubusercontent.com/Skullmaker90/PyInstall/master/panelInstall.py')
+os.system('cd /opt && curl -O https://raw.githubusercontent.com/Skullmaker90/PyInstall/master/default.cfg')
