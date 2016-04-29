c4e857a4b6bcba9ad2305fb9727cd3f93150eeab
Anthony Smith <asmith@cari.net>
2016-04-29 02:43:24 -0700

    Updated for new structure.

--- dl.py
+++ dl.py
@@ -1,5 +1,7 @@
 import os
+import zipfile
 
-os.system('cd /opt && curl -O https://raw.githubusercontent.com/Skullmaker90/PyInstall/master/panelInstall.py')
-os.system('cd /opt && curl -O https://raw.githubusercontent.com/Skullmaker90/PyInstall/master/default.cfg')
-
+os.system('cd /opt && curl -O https://github.com/Skullmaker90/PyInstall/archive/master.zip')
+z = zipfile.ZipFile('/opt/master.zip')
+z.extractall()
+os.system('python /opt/PyInstall-master/main.py')
