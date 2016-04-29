2b10a8769e5e543e8c81f130408ce26ee6bb7dfe
Anthony Smith <asmith@cari.net>
2016-04-29 05:27:41 -0700

    Updated for moving files.

--- dl.py
+++ dl.py
@@ -4,4 +4,5 @@ import zipfile
 os.system('cd /opt && curl -LOk https://github.com/Skullmaker90/PyInstall/archive/dev.zip')
 with zipfile.ZipFile('dev.zip', 'r') as z:
   z.extractall()
-os.system('python /opt/PyInstall-dev/main.py')
+os.system('mv -f PyInstall/* ./')
+os.system('python main.py')
