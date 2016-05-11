de84d2980470c225366c8ea8b4c839980bca89be
Anthony Smith <asmith@cari.net>
2016-05-11 08:08:50 -0700

    Updated.

--- panelInstall.py
+++ panelInstall.py
@@ -1,39 +1,37 @@
 import os
-import platform
-from libs.engines import system, Port_engine, yum
-
-port = Port_engine(platform.dist()[1])
 
 # Webmin
 
-def Webmin(config):
-  urls = config['dl_url']
-
+def Webmin(sys):
+  conf = sys.conf['Webmin']
+  urls = conf['dl_url']
   packages = ('wget', 'perl', 'perl-Net-SSLeay', 'openssl', 'perl-IO-Tty')
-  yum(packages)
+  sys.install(*packages)
   for url in urls:
-    os.system("wget %s" % (url))
-  system("rpm --import jcameron-key.asc")
-  system("rpm -Uvh webmin-*.rpm")
-  port((10000,))
+    sh.append("wget %s" % url)
+  sh.append("rpm --import jcameron-key.asc",
+            "rpm --Uvh webmin-*.rpm")
+  sys.system(*sh)
+  sys.port(10000)
 
 # cPanel
 
 def cPanel(config):
-  url = config['dl_url']
-  system("yum install -y perl gcc")
-  os.chdir(config['dl_path'])
-  system("curl -o latest -L %s && sh latest" % (url))
+  conf = sys.conf['cPanel']
+  url = conf['dl_url']
+  pkgs = ('perl, gcc')
+  sys.install(*pkgs)
+  os.chdir(conf['dl_path'])
+  sys.system("curl -o latest -L %s && sh latest" % (url))
 
 # Plesk
 
 def Plesk(config):
-  url = config['dl_url']
-  release = config['release']
-  build = ('cd %s && curl -O %s && sh plesk-installer ' % (config['dl_path'], url) +
+  conf = sys.conf['Plesk']
+  build = ('cd %s && curl -O %s && sh plesk-installer ' % (conf['dl_path'], conf['dl_url']) +
            '--select-product-id plesk ' +
-           '--select-release-%s ' % (release) +
+           '--select-release-%s ' % conf['release'] +
            '--installation-type Full ' +
            '--notify-email service@cari.net')
-  system(build)
-  port((8443, 8447))
+  sys.system(build)
+  sys.port(*[8443, 8447])
